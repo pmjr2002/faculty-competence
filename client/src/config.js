128383ff5aa8dd3ff8294aa01979e9f40240e9ef
Abhinav Bagalkot <abhinavb.is21@rvce.edu.in>
2024-09-28 13:34:39 +0530

    Updated Config

--- client/src/config.js
+++ client/src/config.js
@@ -1,5 +1,5 @@
 const config = {
-  apiBaseUrl: 'http://localhost:5000/api', // Your deployed API base URL
+  apiBaseUrl: 'https://faculty-competence.onrender.com/api', // Your deployed API base URL
 };
 
 export default config;
